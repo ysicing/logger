4d9a31e40bf24a20419c8ad8c2cf447bb3f9acdc
ysicing <i@ysicing.me>
2020-08-27 13:29:40 +0800

    improvement: init

---
+++ example/example.go
@@ -0,0 +1,18 @@
+// MIT License
+// Copyright (c) 2020 ysicing <i@ysicing.me>
+
+package main
+
+import "github.com/ysicing/logger"
+
+func init() {
+	cfg := logger.LogConfig{Simple: true}
+	logger.InitLogger(&cfg)
+}
+
+func main() {
+	logger.Debug("debug")
+	logger.Info("info")
+	logger.Error("error")
+	logger.Exit("error exit")
+}
